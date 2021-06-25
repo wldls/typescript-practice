06d05a3f347524435b17938705a73abbba122a31
jiin <jiin@devmon.co.kr>
2021-06-25 17:37:16 +0900

    변수와 함수 타입 정의하기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,26 @@
+// JS 문자열 선언
+// let str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플 (배열의 특정 인덱스에 타입 지정)
+let address: [string, number] = ['gangnam', 1];
+
+// TS 객체
+let obj: object = {};
+let person: {name: string, age: number} = {
+	name: 'capt',
+	age: 100,
+};
+
+// TS 진위값
+let show: boolean = true;
